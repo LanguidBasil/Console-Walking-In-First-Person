14fa0270da2eb1c59e8b4be385d7b6bf9af4ddfe
LanguidBasil <e.makunev@gmail.com>
2022-04-22 22:34:55 +1000

    Add operator== to Vectors

--- ConsoleWalkingInFirstPerson/src/Vector2.cpp
+++ ConsoleWalkingInFirstPerson/src/Vector2.cpp
@@ -8,30 +8,35 @@ Vector2f::Vector2f(float x, float y)
 	: X(x), Y(y)
 {}
 
-const Vector2f Vector2f::Add(const Vector2f& lhs, const Vector2f& rhs)
+const Vector2f	Vector2f::Add(const Vector2f& lhs, const Vector2f& rhs)
 {
 	return { lhs.X + rhs.X, lhs.Y + rhs.Y };
 }
-
-const Vector2f Vector2f::Subtract(const Vector2f& lhs, const Vector2f& rhs)
+const Vector2f	Vector2f::Subtract(const Vector2f& lhs, const Vector2f& rhs)
 {
 	return { lhs.X - rhs.X, lhs.Y - rhs.Y };
 }
+const bool		Vector2f::Equals(const Vector2f& lhs, const Vector2f& rhs)
+{
+	return lhs.X == rhs.X && lhs.Y == rhs.Y;
+}
 
-const Vector2f Vector2f::operator+ (const Vector2f& other) const { return Add(*this, other); }
-const Vector2f Vector2f::operator- (const Vector2f& other) const { return Subtract(*this, other); }
-void Vector2f::operator+=(const Vector2f& other)
+const Vector2f	Vector2f::operator+  (const Vector2f& other) const { return Add(*this, other); }
+const Vector2f	Vector2f::operator-  (const Vector2f& other) const { return Subtract(*this, other); }
+const bool		Vector2f::operator== (const Vector2f& other) const { return Equals(*this, other); }
+void			Vector2f::operator+= (const Vector2f& other)
 {
 	auto temp = Add(*this, other);
 	X = temp.X;
 	Y = temp.Y;
 }
-void Vector2f::operator-=(const Vector2f& other)
+void			Vector2f::operator-= (const Vector2f& other)
 {
 	auto temp = Subtract(*this, other);
 	X = temp.X;
 	Y = temp.Y;
 }
+
 
 
 Vector2n::Vector2n()
@@ -42,24 +47,29 @@ Vector2n::Vector2n(int x, int y)
 	: X(x), Y(y)
 {}
 
-const Vector2n Vector2n::Add(const Vector2n& lhs, const Vector2n& rhs)
+const Vector2n	Vector2n::Add(const Vector2n& lhs, const Vector2n& rhs)
 {
 	return { lhs.X + rhs.X, lhs.Y + rhs.Y };
 }
-const Vector2n Vector2n::Subtract(const Vector2n& lhs, const Vector2n& rhs)
+const Vector2n	Vector2n::Subtract(const Vector2n& lhs, const Vector2n& rhs)
 {
 	return { lhs.X - rhs.X, lhs.Y - rhs.Y };
 }
+const bool		Vector2n::Equals(const Vector2n& lhs, const Vector2n& rhs)
+{
+	return lhs.X == rhs.X && lhs.Y == rhs.Y;
+}
 
-const Vector2n Vector2n::operator+ (const Vector2n& other) const { return Add(*this, other); }
-const Vector2n Vector2n::operator- (const Vector2n& other) const { return Subtract(*this, other); }
-void Vector2n::operator+= (const Vector2n& other)
+const Vector2n	Vector2n::operator+  (const Vector2n& other) const { return Add(*this, other); }
+const Vector2n	Vector2n::operator-  (const Vector2n& other) const { return Subtract(*this, other); }
+const bool		Vector2n::operator== (const Vector2n& other) const { return Equals(*this, other); }
+void			Vector2n::operator+= (const Vector2n& other)
 {
 	auto temp = Add(*this, other);
 	X = temp.X;
 	Y = temp.Y;
 }
-void Vector2n::operator-= (const Vector2n& other)
+void			Vector2n::operator-= (const Vector2n& other)
 {
 	auto temp = Subtract(*this, other);
 	X = temp.X;
--- ConsoleWalkingInFirstPerson/src/Vector2.h
+++ ConsoleWalkingInFirstPerson/src/Vector2.h
@@ -8,13 +8,15 @@ struct Vector2f
 	Vector2f();
 	Vector2f(float x, float y);
 
-	static const Vector2f Add(const Vector2f& lhs, const Vector2f& rhs);
-	static const Vector2f Subtract(const Vector2f& lhs, const Vector2f& rhs);
+	static const Vector2f	Add(const Vector2f& lhs, const Vector2f& rhs);
+	static const Vector2f	Subtract(const Vector2f& lhs, const Vector2f& rhs);
+	static const bool		Equals(const Vector2f& lhs, const Vector2f& rhs);
 
-	const Vector2f operator+ (const Vector2f& other) const;
-	const Vector2f operator- (const Vector2f& other) const;
-	void operator+= (const Vector2f& other);
-	void operator-= (const Vector2f& other);
+	const Vector2f	operator+  (const Vector2f& other) const;
+	const Vector2f	operator-  (const Vector2f& other) const;
+	void			operator+= (const Vector2f& other);
+	void			operator-= (const Vector2f& other);
+	const bool		operator== (const Vector2f& other) const;
 };
 
 struct Vector2n
@@ -25,11 +27,13 @@ struct Vector2n
 	Vector2n();
 	Vector2n(int x, int y);
 
-	static const Vector2n Add(const Vector2n& lhs, const Vector2n& rhs);
-	static const Vector2n Subtract(const Vector2n& lhs, const Vector2n& rhs);
+	static const Vector2n	Add(const Vector2n& lhs, const Vector2n& rhs);
+	static const Vector2n	Subtract(const Vector2n& lhs, const Vector2n& rhs);
+	static const bool		Equals(const Vector2n& lhs, const Vector2n& rhs);
 
-	const Vector2n operator+ (const Vector2n& other) const;
-	const Vector2n operator- (const Vector2n& other) const;
-	void operator+= (const Vector2n& other);
-	void operator-= (const Vector2n& other);
+	const Vector2n	operator+  (const Vector2n& other) const;
+	const Vector2n	operator-  (const Vector2n& other) const;
+	void			operator+= (const Vector2n& other);
+	void			operator-= (const Vector2n& other);
+	const bool		operator== (const Vector2n& other) const;
 };
